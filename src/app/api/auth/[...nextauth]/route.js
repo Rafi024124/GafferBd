3606aab8831d458d42ee91ff9ecf7a3d82250cc3
Rafi <md.rafi888444@gmail.com>
2025-08-22 19:25:37 +0600

    lib

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,4 @@
-import { loginUser } from "@/app/lib/loginUser";
+import { loginUser } from "../../../lib/loginUser";
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
